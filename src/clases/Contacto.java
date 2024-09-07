c4b6392979142257fda59d3644f47d9d23913002
jgouffier <gouffierjulian@gmail.com>
2024-09-07 11:22:57 -0300

    Vistas y clases

---
+++ src/clases/Contacto.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+/**
+ *
+ * @author gouff
+ */
+public class Contacto {
+    private String nombre;
+    private Integer dni;
+    private String apellido;
+    private String direccion;
+    private String ciudad;
+    private Long telefono;
+}
---
+++ src/clases/DirectorioTelefonico.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author gouff
+ */
+public class DirectorioTelefonico {
+    private List<Contacto> listaContactos;
+
+    public DirectorioTelefonico() {
+        this.listaContactos = new ArrayList<>();
+    }
+}
+
